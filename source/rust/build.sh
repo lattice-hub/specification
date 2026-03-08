1d677c26f4c59193812c987fc4824525a6259d6a
chuntaojun <liaochuntao@live.com>
2026-03-08 22:13:18 +0800

    first commit

--- source/rust/build.sh
+++ source/rust/build.sh
@@ -23,6 +23,7 @@ popd
 model_dir=${workdir}/api/v1/model
 service_manage_dir=${workdir}/api/v1/service_manage
 traffic_manage_dir=${workdir}/api/v1/traffic_manage
+ratelimiter_dir=${workdir}/api/v1/traffic_manage/ratelimiter
 fault_tolerance_dir=${workdir}/api/v1/fault_tolerance
 config_manage_dir=${workdir}/api/v1/config_manage
 security_dir=${workdir}/api/v1/security
@@ -32,6 +33,7 @@ rust_root_dir=${workdir}/source/rust/pole-specification
 cp ${model_dir}/*.proto ${rust_root_dir}/proto/
 cp ${service_manage_dir}/*.proto ${rust_root_dir}/proto/
 cp ${traffic_manage_dir}/*.proto ${rust_root_dir}/proto/
+cp ${ratelimiter_dir}/*.proto ${rust_root_dir}/proto/
 cp ${fault_tolerance_dir}/*.proto ${rust_root_dir}/proto/
 cp ${config_manage_dir}/*.proto ${rust_root_dir}/proto/
 cp ${security_dir}/*.proto ${rust_root_dir}/proto/
